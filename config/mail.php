1427b1637c50850b1a9ca6a91d09571e23a484b3
carlos martin <cjmar87@gmail.com>
2018-10-04 16:46:08 -0500

    cambiando variables globales

--- config/mail.php
+++ config/mail.php
@@ -56,8 +56,8 @@ return [
     */
 
     'from' => [
-        'address' => env('MAIL_FROM_ADDRESS', 'hello@example.com'),
-        'name' => env('MAIL_FROM_NAME', 'Example'),
+        'address' => env('MAIL_FROM_ADDRESS', 'hola@elnidodelpajaro.club'),
+        'name' => env('MAIL_FROM_NAME', 'El nido del pájaro'),
     ],
 
     /*
